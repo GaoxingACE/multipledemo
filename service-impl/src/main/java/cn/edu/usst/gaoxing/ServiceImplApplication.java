0902b7491445a1affc62c929bdd2b3ab7120b85e
gaoxilong <gaoxilong@cmos.chinamobile.com>
2019-07-16 17:19:34 +0800

    1、提高spring boot版本。 2、通过启动类注解配置修改，将基于xml文件的dubbo配置，改为在application.properties中配置相关属性，取消基于xml的配置。

--- service-impl/src/main/java/cn/edu/usst/gaoxing/ServiceImplApplication.java
+++ service-impl/src/main/java/cn/edu/usst/gaoxing/ServiceImplApplication.java
@@ -4,13 +4,15 @@ package  cn.edu.usst.gaoxing;
  *@Author GaoXilong
  *@Date 2019/7/10 12:52
  */
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
+@EnableDubbo
 @SpringBootApplication
-@ImportResource("classpath:provider.xml")
+/*@ImportResource("classpath:provider.xml")*/
 @MapperScan("cn.edu.usst.gaoxing.dao")
 public class ServiceImplApplication {
 
--- web/src/main/java/cn/edu/usst/gaoxing/WebApplication.java
+++ web/src/main/java/cn/edu/usst/gaoxing/WebApplication.java
@@ -1,13 +1,15 @@
 package cn.edu.usst.gaoxing;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
+@EnableDubbo
 @SpringBootApplication
-@ImportResource("classpath:consumer.xml")
+/*@ImportResource("classpath:consumer.xml")*/
 public class WebApplication {
 
     public static void main(String[] args) {
